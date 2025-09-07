ef307c6b027040aec905fdebd0a381a8302f91b5
Greg James <gregvjames99@gmail.com>
2025-09-07 01:52:12 -0500

    Add SubstitutionConfigurationProvider

---
+++ Configuration.Substitution/SubstitutionConfigurationProvider.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
+
+namespace Configuration.Substitution;
+
+public class SubstitutionConfigurationProvider: ConfigurationProvider
+{
+    private readonly IConfigurationRoot _configuration;
+    private static readonly Regex PlaceholderRegex = new(@"\$\{([^\}]+)\}", RegexOptions.Compiled);
+    public SubstitutionConfigurationProvider(IConfigurationRoot configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public override void Load()
+    {
+        foreach (var kvp in _configuration.AsEnumerable())
+        {
+            if (kvp.Value is not null)
+            {
+                var resolved = SubstitutePlaceholder(kvp.Value);
+                Data[kvp.Key] = resolved;
+            }
+        }
+    }
+
+    private string SubstitutePlaceholder(string placeholder)
+    {
+        return PlaceholderRegex.Replace(placeholder, match =>
+        {
+            var key = match.Groups[1].Value;
+            return _configuration[key] ?? match.Value;
+        });
+    }
+}
